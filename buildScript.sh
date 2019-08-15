eaeeb4ed0f68d1f3560b992d351ecf45a38a8189
Yanlong-li <ahlyl94@gmail.com>
2019-08-15 15:45:48 +0800

    初始化VUE框架+增加编译脚本

---
+++ buildScript.sh
@@ -0,0 +1,19 @@
+# 编译脚本
+
+logUpdatePath="log/update"
+logBuildPath="log/build"
+# 更新依赖
+logDate=$(date  +"%Y-%m-%d-%H-%M")
+if [ ! -d "$logUpdatePath" ]; then
+    mkdir -p $logUpdatePath
+fi
+if [ ! -d "$logBuildPath" ]; then
+    mkdir -p $logBuildPath
+fi
+echo "更新依赖"
+npm install >$logUpdatePath/$logDate.log
+echo "编译项目"
+npm run build >$logBuildPath/$logDate.log
+echo "设置权限"
+chown -R www:www dist
+echo "运行结束"
