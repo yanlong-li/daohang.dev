45fb836b7271dbd5c7c540333142e2a5febd0ac1
Yanlong-li <ahlyl94@gmail.com>
2019-08-15 15:54:27 +0800

    更新PWA配置

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,10 @@
 module.exports = {
     // ...other vue-cli plugin options...
     pwa: {
-        name: 'My App',
+        name: '开发导航',
         themeColor: '#4DBA87',
         msTileColor: '#000000',
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
-
-        // configure the workbox plugin
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            swSrc: 'src/registerServiceWorker.js',
-            // ...other Workbox options...
-        }
     },
 };
