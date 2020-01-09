045d6122c8adf8b166f62a8028e2ca371dceb4db
Yanlong-li <ahlyl94@gmail.com>
2020-01-09 13:40:23 +0800

    修复被覆盖的文件
    Signed-off-by: Yanlong-li <ahlyl94@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,107 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header>
+        <TopNav></TopNav>
+      </el-header>
+      <el-container>
+        <el-aside>
+          <AsideNav></AsideNav>
+        </el-aside>
+        <el-container>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+        </el-container>
+      </el-container>
+      <el-footer>
+        <Footer></Footer>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
-<style scoped lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        /*color: #2c3e50;*/
+<script>
+  import TopNav from './components/TopNav'
+  import Footer from './components/Footer'
+  import AsideNav from './components/AsideNav'
 
-        .el-header {
-            padding: 0;
-            border-bottom: 1px solid #D3DCE6;
-        }
-
-        .el-header, .el-footer {
-            /*background-color: #B3C0D1;*/
-            color: #333;
-            text-align: center;
-            line-height: 60px;
-        }
-
-        .el-footer {
-            background-color: #f8f9fa;
-            /*position: fixed;*/
-            /*bottom: 0;*/
-            width: 100%;
-        }
-
-        .el-aside {
-            background-color: #D3DCE6;
-            color: #333;
-            text-align: center;
-            /*line-height: 200px;*/
-        }
-
-        .el-main {
-            /*background-color: #E9EEF3;*/
-            color: #333;
-            text-align: center;
-            /*line-height: 160px;*/
-            min-height: 600px;
-        }
-
-        .el-container:nth-child(5) .el-aside,
-        .el-container:nth-child(6) .el-aside {
-            /*line-height: 260px;*/
-        }
-
-        .el-container:nth-child(7) .el-aside {
-            /*line-height: 320px;*/
-        }
-
+  export default {
+    name: 'app',
+    components: {
+      TopNav,
+      Footer,
+      AsideNav
+    },
+    created () {
+      // this.$router.push({
+      //   name:"home"
+      // })
     }
+  }
+</script>
+
+<style scoped lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    /*color: #2c3e50;*/
+
+    .el-header {
+      padding: 0;
+      border-bottom: 1px solid #D3DCE6;
+    }
+
+    .el-header, .el-footer {
+      /*background-color: #B3C0D1;*/
+      color: #333;
+      text-align: center;
+      line-height: 60px;
+    }
+
+    .el-footer {
+      background-color: #f8f9fa;
+      /*position: fixed;*/
+      /*bottom: 0;*/
+      width: 100%;
+    }
+
+    .el-aside {
+      background-color: #D3DCE6;
+      color: #333;
+      text-align: center;
+      /*line-height: 200px;*/
+    }
+
+    .el-main {
+      /*background-color: #E9EEF3;*/
+      color: #333;
+      text-align: center;
+      /*line-height: 160px;*/
+      min-height: 600px;
+    }
+
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+      /*line-height: 260px;*/
+    }
+
+    .el-container:nth-child(7) .el-aside {
+      /*line-height: 320px;*/
+    }
+
+  }
 </style>
 <style>
-    * {
-        padding: 0;
-        margin: 0;
-        font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
-    }
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  }
 
-    body {
-        min-width: 1366px;
-    }
+  body {
+    min-width: 1366px;
+  }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>关于我们</h1>
+    <div class="content">
+      还没想好怎么描述
+    </div>
   </div>
 </template>
+<style scoped lang="scss">
+  .about {
+    h1{
+      margin-bottom: 20px;
+    }
+    /*background: rebeccapurple;*/
+    .content {
+
+    }
+  }
+</style>
